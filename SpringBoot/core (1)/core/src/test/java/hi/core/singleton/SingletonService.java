c349eba0f6552f6cf47d9cb610e1ce58279a198f
haemiii <73984822+haemiii@users.noreply.github.com>
2023-01-24 23:09:08 +0900

    싱글톤 컨테이너
    - 스프링 컨테이너 : 싱글톤 패턴 문제 해결, 인스턴스 싱글톤 관리

--- SpringBoot/core (1)/core/src/test/java/hi/core/singleton/SingletonService.java
+++ SpringBoot/core (1)/core/src/test/java/hi/core/singleton/SingletonService.java
@@ -1,8 +1,12 @@
 package hi.core.singleton;
 
+import hi.core.AppConfig;
+import hi.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -45,5 +49,27 @@ public class SingletonService {
         // singletonService1 == singletonService2
         assertThat(singletonService1).isSameAs(singletonService2);
         singletonService1.logic();
+    }
+
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1.조회 : 호출 할 때마다 같은 객체 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2.조회 : 호출 할 때마다 같은 객체 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // singletonService1 == singletonService2
+        assertThat(memberService1).isSameAs(memberService2);
+
     }
 }
