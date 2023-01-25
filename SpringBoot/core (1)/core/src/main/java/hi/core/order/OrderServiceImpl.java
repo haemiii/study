09d4ce9e996081352ef5de62185053e972b93f93
haemiii <73984822+haemiii@users.noreply.github.com>
2023-01-25 21:35:57 +0900

    의존관계 주입 방식
    - 생성자, setter, 필드 , 메서드 주입 방식

--- SpringBoot/core (1)/core/src/main/java/hi/core/order/OrderServiceImpl.java
+++ SpringBoot/core (1)/core/src/main/java/hi/core/order/OrderServiceImpl.java
@@ -14,14 +14,39 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService{
 
     private MemberRepository memberRepository;
-    // DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private DiscountPolicy discountPolicy;
+    // DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    // 1.생성자 방식의 의존관계 주입
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // 2.setter 방식의 의존관계 주입 : 자바빈 프로퍼티 규약
+//    @Autowired(required = false)  //required = false : 주입할 대상이 없어도 동작
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // 3.필드 주입 방식
+//    @Autowired
+//    private MemberRepository memberRepository;
+//    @Autowired
+//    private DiscountPolicy discountPolicy;
+
+    // 4.메서드 주입 방식
+    /*@Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
     public MemberRepository getMemberRepository() {
         return memberRepository;
