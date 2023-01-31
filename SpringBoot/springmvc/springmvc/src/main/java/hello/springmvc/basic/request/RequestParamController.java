69c11ec5c0a910bb8d047673ebf547d597126151
haemiii <73984822+haemiii@users.noreply.github.com>
2023-01-31 11:48:56 +0900

    HTTP 요청 파라미터
    -ModelAttribute 사용

--- SpringBoot/springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ SpringBoot/springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -72,5 +72,29 @@ public class RequestParamController {
         return "ok";
     }
 
+//    @ResponseBody
+//    @RequestMapping("/model-attribute-v1")
+//    public String modelAttributeV1(@RequestParam String username, @RequestParam int age) {
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+//
+//        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+//        log.info("helloData={}", helloData)
+//        return "ok";
+//    }
 
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
 }
