194b9720976026acfb477b76bf20a24f31a95031
haemiii <73984822+haemiii@users.noreply.github.com>
2023-01-31 11:46:37 +0900

    HTTP 요청 파라미터 - 쿼리, HTML Form
    - requeset.getParameter()

--- SpringBoot/springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ SpringBoot/springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,2 +1,26 @@
-package hello.springmvc.basic.request;public class RequestParamController {
+package hello.springmvc.basic.request;
+
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("username"));
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("ok");
+    }
+    
 }
